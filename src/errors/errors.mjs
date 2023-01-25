ce7b8247de3705aecc82692ca27c1e639182ef6b
Codevendor <adam@codevendor.com>
2023-01-25 11:22:20 -0700

    Adding some error types

---
+++ src/errors/errors.mjs
@@ -0,0 +1,10 @@
+/** 
+ * The error types for inferjs.
+ * @category errors
+ * @module inferjs
+ */
+
+// Error Types
+export { InferError } from "./infer-error.mjs";
+export { InferTypeError } from "./infer-type-error.mjs";
+export { InferUnhandledTagError } from "./infer-unhandled-tag-error.mjs";
